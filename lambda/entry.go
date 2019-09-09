57acb56bff6f7ae9e71bb76f4c4f90ab6f147725
AmFlint <amasselot@hotmail.com>
2019-09-09 22:33:41 +0200

    [Runtime/Update] Update runtime to use HTTP Server, Invoke functions with requests events

--- lambda/entry.go
+++ lambda/entry.go
@@ -1,17 +1,28 @@
 package lambda
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/scaleway/scaleway-functions-go/events"
 )
 
-const defaultPort = 8080
+const defaultPort = 8081
 
 // FunctionHandler - Handler for Event
 type FunctionHandler func(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
+
+// Request structure sent from core runtime
+type runtimeRequest struct {
+	Event   interface{}
+	Context interface{}
+}
 
 // Start - Start the process
 func Start(handler interface{}) {
@@ -19,20 +30,64 @@ func Start(handler interface{}) {
 	StartHandler(wrappedHandler)
 }
 
-// StartHandler - Execute Handler
+// StartHandler - Execute a Function handler
 func StartHandler(handler Handler) {
-	// 1: Parse arguments
-	event := os.Args[1]
-	// context := os.Args[4]
-
-	// 2:
-	response, err := handler.Invoke(nil, []byte(event))
+	portEnv := os.Getenv("SCW_UPSTREAM_PORT")
+	port, err := strconv.Atoi(portEnv)
 	if err != nil {
-		errorMessage := fmt.Sprintf("[SCW_ERROR] %s", err.Error())
-		log.Print(errorMessage)
-		return
+		port = defaultPort
 	}
 
-	successMessage := fmt.Sprintf("[SCW_END] %s", string(response))
-	fmt.Print(successMessage)
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", port),
+		ReadTimeout:    3 * time.Second,
+		WriteTimeout:   3 * time.Second,
+		MaxHeaderBytes: 1 << 20, // Max header of 1MB
+	}
+
+	http.HandleFunc("/", makeRequestHandler(handler))
+	log.Fatal(s.ListenAndServe())
+}
+
+// Create HTTP Handler, in charge of retrieving events and execution context from upstream requests,
+// Invoke a function Handler, and handle responses (error or success)
+func makeRequestHandler(handler Handler) func(res http.ResponseWriter, req *http.Request) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		bodyBytes, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			errorMessage := fmt.Sprintf("An error occured while reading request: %v", err)
+			handleResponse(res, http.StatusInternalServerError, []byte(errorMessage))
+			return
+		}
+
+		var runtimeReq runtimeRequest
+
+		if err := json.Unmarshal(bodyBytes, &runtimeReq); err != nil {
+			errorMessage := fmt.Sprintf("Unable to parse Request body: %v", err)
+			handleResponse(res, http.StatusInternalServerError, []byte(errorMessage))
+			return
+		}
+
+		// Transform event to []byte for later use
+		eventBytes, err := json.Marshal(runtimeReq.Event)
+		if err != nil {
+			handleResponse(res, http.StatusInternalServerError, []byte("fucked"))
+			return
+		}
+
+		// Invoke function Handler
+		response, err := handler.Invoke(nil, eventBytes)
+		if err != nil {
+			errorMessage := fmt.Sprintf("An error occured during handler execution: %v", err)
+			handleResponse(res, http.StatusInternalServerError, []byte(errorMessage))
+			return
+		}
+
+		handleResponse(res, http.StatusOK, response)
+	}
+}
+
+func handleResponse(res http.ResponseWriter, statusCode int, message []byte) {
+	res.WriteHeader(statusCode)
+	res.Write(message)
 }
