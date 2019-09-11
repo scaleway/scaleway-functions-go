72940b7acd2fb120a8b723077b9d0a1f4720e49e
AmFlint <amasselot@hotmail.com>
2019-09-11 15:50:30 +0200

    Fix according to coments

--- lambda/handler.go
+++ lambda/handler.go
@@ -12,12 +12,12 @@ type Handler interface {
 	Invoke(ctx context.Context, payload []byte) ([]byte, error)
 }
 
-// lambdaHandler is the generic function type
-type lambdaHandler func(context.Context, []byte) (interface{}, error)
+// functionHandler is the generic function type
+type functionHandler func(context.Context, []byte) (interface{}, error)
 
 // Invoke calls the handler, and serializes the response.
 // If the underlying handler returned an error, or an error occurs during serialization, error is returned.
-func (handler lambdaHandler) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
+func (handler functionHandler) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
 	response, err := handler(ctx, payload)
 	if err != nil {
 		return nil, err
@@ -31,49 +31,14 @@ func (handler lambdaHandler) Invoke(ctx context.Context, payload []byte) ([]byte
 	return responseBytes, nil
 }
 
-func errorHandler(e error) lambdaHandler {
+func errorHandler(e error) functionHandler {
 	return func(ctx context.Context, event []byte) (interface{}, error) {
 		return nil, e
 	}
 }
 
-func validateArguments(handler reflect.Type) (bool, error) {
-	handlerTakesContext := false
-	if handler.NumIn() > 2 {
-		return false, fmt.Errorf("handlers may not take more than two arguments, but handler takes %d", handler.NumIn())
-	} else if handler.NumIn() > 0 {
-		contextType := reflect.TypeOf((*context.Context)(nil)).Elem()
-		argumentType := handler.In(0)
-		handlerTakesContext = argumentType.Implements(contextType)
-		if handler.NumIn() > 1 && !handlerTakesContext {
-			return false, fmt.Errorf("handler takes two arguments, but the first is not Context. got %s", argumentType.Kind())
-		}
-	}
-
-	return handlerTakesContext, nil
-}
-
-func validateReturns(handler reflect.Type) error {
-	errorType := reflect.TypeOf((*error)(nil)).Elem()
-
-	switch n := handler.NumOut(); {
-	case n > 2:
-		return fmt.Errorf("handler may not return more than two values")
-	case n > 1:
-		if !handler.Out(1).Implements(errorType) {
-			return fmt.Errorf("handler returns two values, but the second does not implement error")
-		}
-	case n == 1:
-		if !handler.Out(0).Implements(errorType) {
-			return fmt.Errorf("handler returns a single value, but it does not implement error")
-		}
-	}
-
-	return nil
-}
-
 // NewHandler creates a base lambda handler from the given handler function. The
-// returned Handler performs JSON deserialization and deserialization, and
+// returned Handler performs JSON serialization and deserialization, and
 // delegates to the input handler function.  The handler function parameter must
 // satisfy the rules documented by Start.  If handlerFunc is not a valid
 // handler, the returned Handler simply reports the validation error.
@@ -87,23 +52,11 @@ func NewHandler(handlerFunc interface{}) Handler {
 		return errorHandler(fmt.Errorf("handler kind %s is not %s", handlerType.Kind(), reflect.Func))
 	}
 
-	takesContext, err := validateArguments(handlerType)
-	if err != nil {
-		return errorHandler(err)
-	}
-
-	if err := validateReturns(handlerType); err != nil {
-		return errorHandler(err)
-	}
-
-	return lambdaHandler(func(ctx context.Context, payload []byte) (interface{}, error) {
+	return functionHandler(func(ctx context.Context, payload []byte) (interface{}, error) {
 
 		// construct arguments
 		var args []reflect.Value
-		if takesContext {
-			args = append(args, reflect.ValueOf(ctx))
-		}
-		if (handlerType.NumIn() == 1 && !takesContext) || handlerType.NumIn() == 2 {
+		if (handlerType.NumIn() == 1) || handlerType.NumIn() == 2 {
 			eventType := handlerType.In(handlerType.NumIn() - 1)
 			event := reflect.New(eventType)
 
